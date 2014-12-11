d645c18ed424a3d8148dff402e5663dbd3c0bfde
Murali Kashaboina <kbmuralidhar@gmail.com>
2014-12-10 23:54:05 -0600

    Modified the programming assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,13 @@
-## This function is a convenient wrapper function that returns a list of 
-## accessor functions to get & set an invertible matrix and its inverse 
-## matrix. Specifically this function returns a named list with following
-## accessor functions: 
+## This function is a convenient wrapper function that returns a 'special matrix' 
+## containing get & set accessor functions for the invertible matrix and its matrix inverse. 
+## Specifically this function returns a named list with following accessor functions: 
 ## 
 ## set : function to set a reference to invertible square matrix
 ## get : function to get a reference to invertible square matrix
 ## setinverse : function to set a reference to inverse of the square matrix 
 ## getinverse : function to get a reference to inverse of the square matrix
 ##
-## A convenient function to set & get a matrix and its inverse
+## A convenient function that creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 	x_inv <- NULL
@@ -31,12 +30,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## This function takes the named list of accessors for a given matrix as 
-## created by 'makeCacheMatrix' function and solves for the inverse of the
-## matrix. it first checks to see if matrix inverse has already been solved. 
-## If so, it just returns the matrix inverse from the cache and skips the computation. 
-## Otherwise, it computes the matrix inverse, stores it in the cache and 
-## returns the computed inverse
+## This function takes the 'special matrix' as created by 'makeCacheMatrix' function 
+## and solves for the inverse of the matrix. It first checks to see if matrix inverse 
+## has already been solved. If so, it just returns the matrix inverse from the cache 
+## and skips the computation. Otherwise, it computes the matrix inverse, caches it 
+## and returns the computed inverse
 
 cacheSolve <- function(x, ...) {
 	x_inverse <- x$getinverse()
